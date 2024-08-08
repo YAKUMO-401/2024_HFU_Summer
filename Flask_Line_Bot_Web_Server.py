7f602773a9bef8f9e44da527b380755019b4bbd6
unknown <blackgoldsaw401@gmail.com>
2024-08-08 14:27:27 +0800

    [Linr BOT]

---
+++ Flask_Line_Bot_Web_Server.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+import os,sys
+
+app = Flask(__name__)
+
+channel_secret = os.getenv('LINEBOT_SECRET_KEY', None)
+channel_access_token = os.getenv('LINEBOT_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINEBOT_SECRET_KEY as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINEBOT_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+
+configuration = Configuration(access_token='channel_access_token')
+handler = WebhookHandler('channel_secret')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
+
+if __name__ == "__main__":
+    app.run()
